09117edc64a33799b7f9e0d53d81d875701872b5
Georgi Popov <magadanski.uchen@gmail.com>
2014-08-23 12:57:36 +0300

    app initialization updates

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 config = require('./config');
 
 util = require('util');
+
 mysql = require('mysql');
 csv = require('fast-csv');
 fs = require('fs');
 
 EventEmitter = require('events').EventEmitter;
+
 DB = require('db');
+Movie = require('movie');
 MyFavoriteMovies = require('my-favorite-movies');
 
 db = new DB();
-app = new MyFavoriteMovies();
+app = new MyFavoriteMovies({ db: db });
